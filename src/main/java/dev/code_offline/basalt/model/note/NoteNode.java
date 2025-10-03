f3f4e9d6c9360260032458b398237fe1fa36b9a3
Gershaveut <vova1234567890012011@yandex.ru>
2025-10-03 15:52:43 +0300

    исправление ошибок

--- src/main/java/dev/code_offline/basalt/model/note/NoteNode.java
+++ src/main/java/dev/code_offline/basalt/model/note/NoteNode.java
@@ -7,26 +7,42 @@ import org.dyn4j.dynamics.Body;
 import java.util.ArrayList;
 import java.util.List;
 
-public class NoteNode extends Note implements Node {
+public class NoteNode implements Node {
     private final Body body = new Body();
     private final Client client;
-    
+
+    private final long id;
+    private final String name;
+    private final long person;
+    private final List<Long> links;
+
     public NoteNode(Note note, Client client) {
-        super(note.getName(), note.getPerson(), note.getText(), note.getParent(), note.getLinks());
-        
         this.client = client;
-        
-        this.setId(note.getId());
+
+        this.id = note.getId();
+        this.name = note.getName();
+        this.person = note.getPerson();
+        this.links = note.getLinks();
     }
 
     @Override
     public String getName() {
-        return super.getName();
+        return name;
     }
 
     @Override
     public String getAuthor() {
-        return client.getPerson(super.getPerson()).getName();
+        return client.getPerson(person).getName();
+    }
+
+    @Override
+    public List<Long> getLinks() {
+        return links;
+    }
+
+    @Override
+    public long getId() {
+        return id;
     }
 
     @Override
